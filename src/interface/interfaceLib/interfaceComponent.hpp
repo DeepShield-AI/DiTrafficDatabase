1d55b6c4ee601d59144c4aa79f6e993844c7270f
EricXiaozj <2240223100@qq.com>
2026-02-03 03:02:36 +0000

    component base class

---
+++ src/interface/interfaceLib/interfaceComponent.hpp
@@ -0,0 +1,62 @@
+#pragma once
+#include <string>
+#include <atomic>
+#include <unordered_map>
+#include <iostream>
+#include <fstream>
+
+struct InterfaceContext{
+    // TODO: Context implementation
+};
+
+class InterfaceComponent {
+private:
+    std::string componentType;
+    std::string componentName;
+    u_int64_t componentID;
+    std::string logPath;
+    std::atomic_bool running;
+    std::ofstream logFile;
+public:
+    InterfaceComponent(const std::string& type,const std::string& name, const std::string& logPath, const u_int64_t id) : componentType(type), componentName(name),componentID(id), logPath(logPath), running(false){
+        this->logFile.open(this->logPath + "/" + this->componentType + ".log", std::ios::app);
+        // this->logFile.open(this->logPath + "/" + this->componentType + ".log");
+        if(!this->logFile.is_open()){
+            throw std::runtime_error("Failed to open " + this->componentName + " log file");
+        }
+    }
+    virtual ~InterfaceComponent(){
+        if(this->logFile.is_open()){
+            this->logFile.close();
+        }
+    }
+    virtual void init(InterfaceContext& cfg)=0;
+    virtual void run()=0;
+    void start(){
+        this->running.store(true);
+    }
+    void stop(){
+        this->running.store(false);
+    }
+    std::string type() const{
+        return this->componentType;
+    }
+    std::string name() const{
+        return this->componentName;
+    }
+    std::string path() const {
+        return this->logPath;
+    }
+    u_int64_t id() const{
+        return this->componentID;
+    }
+    bool isRunning() const{
+        return this->running.load();
+    }
+    void log(const std::string& message){
+        std::string log_message = "[" + this->componentName + std::to_string(this->componentID) + "] " + message;
+        if(this->logFile.is_open()){
+            this->logFile << log_message << std::endl;
+        }
+    }
+};
